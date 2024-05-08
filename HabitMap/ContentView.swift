101a65b04cbbb2ba428076087d58aa88b74464fd
DevCache1 <martynasbaranskas@gmail.com>
2024-05-08 18:28:17 +0300

    V0.0.1
    Implemented add/edit/delete and storing to UserStorage functionalities.

--- HabitMap/ContentView.swift
+++ HabitMap/ContentView.swift
@@ -9,13 +9,19 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        TabView {
+            OverviewView()
+                .tabItem {
+                    Image(systemName: "house")
+                    Text("Home")
+                }
+
+            HabitsView()
+                .tabItem {
+                    Image(systemName: "list.bullet")
+                    Text("Habits")
+                }
         }
-        .padding()
     }
 }
 
---
+++ HabitMap/Views/OverviewView.swift
@@ -0,0 +1,18 @@
+//
+//  OverviewView.swift
+//  HabitMap
+//
+//  Created by Martynas Baranskas on 08/05/2024.
+//
+
+import SwiftUI
+
+struct OverviewView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OverviewView()
+}
