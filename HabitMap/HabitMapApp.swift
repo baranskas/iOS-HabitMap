96d1dd172281ee5e3740cd1e5c9566c700e3429d
DevCache1 <martynasbaranskas@gmail.com>
2024-05-08 17:20:48 +0300

    Initial Commit

---
+++ HabitMap/HabitMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitMapApp.swift
+//  HabitMap
+//
+//  Created by Martynas Baranskas on 08/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct HabitMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
